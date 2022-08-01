315ec866bb40e44a525b6174f02777e1fa3df8a6
van133 <vkn123@naver.com>
2022-08-01 18:05:59 +0900

    gogo

---
+++ demo/src/main/java/com/SoccerInKorea/demo/ShanApplication.java
@@ -0,0 +1,13 @@
+package com.SoccerInKorea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/SoccerInKorea/demo/URL_Controller.java
@@ -0,0 +1,27 @@
+package com.SoccerInKorea.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class URL_Controller {
+    @GetMapping("Main")
+    public String Main(Model model){
+        model.addAttribute("data","start");
+        return "Main";
+    }
+    @GetMapping("TEMP")
+    public String TEMP(@RequestParam("name") String name,Model model){
+        model.addAttribute("data","start");
+        return "TEMP";
+    }
+
+    @GetMapping("GET")
+    public  String GET(@RequestParam("name") String name, Model mod){
+        mod.addAttribute("name",name);
+        return "GET";
+        }
+
+}
